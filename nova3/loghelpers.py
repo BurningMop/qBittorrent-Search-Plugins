7dbc39f4e5247de6c81a15123c13c3b35cecddfc
Burning Mop <burning.mop@yandex.com>
2025-11-23 01:39:57 -0300

    added pornrips

--- nova3/loghelpers.py
+++ nova3/loghelpers.py
@@ -1,8 +1,9 @@
 from datetime import datetime
 
+import codecs
 
 def save_search_results(engine: str, page: int, html: str):
     now = datetime.today().strftime('%Y%m%d-%H%M%S')
     log_file_name = f'./log/{engine}.page-{page}.{now}.html'
-    with open(log_file_name, "w") as text_file:
-        text_file.write(html)
+    text_file = codecs.open(log_file_name, "w", "utf-8")
+    text_file.write(html)
